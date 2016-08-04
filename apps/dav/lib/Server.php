089236aa8bb3315d87880cda3042f7d3c8980147
Piotr M <mrow4a@yahoo.com>
2016-08-04 14:02:57 +0200

    Feature: Implement bundling using multipart, MultipartContentsParser, BundledFile unit tests with bugfixes Current progress: Handles multipart response

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -173,6 +173,9 @@ class Server {
 				$this->server->addPlugin(
 					new \OCA\DAV\Connector\Sabre\QuotaPlugin($view));
 
+				//TODO:verify if this is a proper place for the plugin
+				$this->server->addPlugin(
+					new \OCA\DAV\Files\BundlingPlugin($view));
 			}
 		});
 	}
