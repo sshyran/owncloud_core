458c8f172009bbe4ae4386b7e844362727898541
Thomas Müller <thomas.mueller@tmit.eu>
2016-09-02 21:28:12 +0200

    - add some padding to the button - use hidden instead of hidden-visually - add tooltip - use notifications on copy of the dav url- tooltip is used for the description of the button

--- core/js/js.js
+++ core/js/js.js
@@ -1926,15 +1926,22 @@ OC.Util = {
 		return false;
 	},
 
-	setupClipboard: function(element) {
+	setupClipboard: function(element, options) {
+		options = options || {};
+		options.notificationMode = options.notificationMode || 'tooltip';
+		options.successMessage = options.successMessage ||  t('core', 'Copied!');
 		var clipboard = new Clipboard(element);
 		clipboard.on('success', function(e) {
-			$input = $(e.trigger);
-			$input.tooltip({placement: 'bottom', trigger: 'manual', title: t('core', 'Copied!')});
-			$input.tooltip('show');
-			_.delay(function() {
-				$input.tooltip('hide');
-			}, 3000);
+			if (options.notificationMode === 'tooltip') {
+				$input = $(e.trigger);
+				$input.tooltip({placement: 'bottom', trigger: 'manual', title: options.successMessage});
+				$input.tooltip('show');
+				_.delay(function() {
+					$input.tooltip('hide');
+				}, 3000);
+			} else {
+				OC.Notification.showTemporary(options.successMessage);
+			}
 		});
 		clipboard.on('error', function (e) {
 			$input = $(e.trigger);
@@ -1947,15 +1954,19 @@ OC.Util = {
 				actionMsg = t('core', 'Press Ctrl-C to copy.');
 			}
 
-			$input.tooltip({
-				placement: 'bottom',
-				trigger: 'manual',
-				title: actionMsg
-			});
-			$input.tooltip('show');
-			_.delay(function () {
-				$input.tooltip('hide');
-			}, 3000);
+			if (options.notificationMode === 'tooltip') {
+				$input.tooltip({
+					placement: 'bottom',
+					trigger: 'manual',
+					title: actionMsg
+				});
+				$input.tooltip('show');
+				_.delay(function () {
+					$input.tooltip('hide');
+				}, 3000);
+			} else {
+				OC.Notification.showTemporary(options.successMessage);
+			}
 		});
 	}
 };
